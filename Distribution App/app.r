d52b804c74f889f393523d27da39bc1eb5387f44
kmanapradit <manaprak@mail.gvsu.edu>
2022-01-11 12:46:39 -0500

    Add binomial and gamma distribution

--- Distribution App/app.r
+++ Distribution App/app.r
@@ -1,41 +1,148 @@
 # Distribution App
+# I added tabs for binomial and gamma distributions.
+# But the random sample's button is still linked to the normal sampling (first tab). 
+# So, there is no change when you click on the random button in the binomial and gamma tabs.
+
 library(shiny)
 library(moments)
-ui <- fluidPage(
-  wellPanel(h2('The Normal Density'),
-            'The effect of the mean and the standard deviation'),
-  fluidRow(
-    column(4, wellPanel(
-      sliderInput('mu', label='Mean', min=0, max=10, value=3, step=.1),
-      sliderInput('sigma', label='Standard Deviation', min=1, 4, value=2, step=.1),
-      checkboxInput('emp', label='Show the Empirical Rule', value=TRUE)
-    )),
-    column(8, wellPanel(
-      plotOutput('normaldist')
-    )),
-    column(4, wellPanel(
-      # random sample
-      numericInput('n', label='Sample Size', min=0, max=10000, value=50, step=1),
-      # buttton to run the sample size
-      actionButton("do", "Random"),
-      # checkbox for displaying density
-      checkboxInput('dens', label='Show the Density Curve', value=FALSE),
-      # Condition to display smoothness when selected the checkbox
-      conditionalPanel(
-        condition="input.dens % 2 == 1",
-        sliderInput('dens_smooth', label='Density Smoothness',
-                    min=0.1, 5, value=0.5, step=.1))
-    )),
-    column(8, wellPanel(
-      plotOutput('simulate'),
-      tableOutput('random_summary')
-    ))
+
+# Function for the random sampling tab
+random_tab <- function(){
+  wellPanel(
+    # Random sample size
+    numericInput('n', label='Sample Size', min=0, max=10000, value=50, step=1),
+    # Button to run the sample size
+    actionButton("do", "Random"),
+    # Checkbox for displaying density
+    checkboxInput('dens', label='Show the Density Curve', value=FALSE),
+    # Condition to display smoothness when selected the checkbox
+    conditionalPanel(
+      condition="input.dens % 2 == 1",
+      sliderInput('dens_smooth', label='Density Smoothness',
+                  min=0.1, 5, value=0.5, step=.1))
   )
+}
+
+
+
+ui <- fluidPage(
+  tabsetPanel(
+    tabPanel("Normal",
+             wellPanel(h2('The Normal Density'),
+                       'The effect of the mean and the standard deviation'),
+             fluidRow(
+               # Input for normal density plot
+               column(4, wellPanel(
+                 sliderInput('mu', label='Mean', min=0, max=10, value=3, step=.1),
+                 sliderInput('sigma', label='Standard Deviation', min=1, 4, value=2, step=.1),
+                 checkboxInput('emp', label='Show the Empirical Rule', value=TRUE)
+               )),
+               # Plot of normal density
+               column(8, wellPanel(
+                 plotOutput('normaldist')
+               )),
+               # Input for sampling
+               column(4, random_tab()
+               ),
+               # Plot of simulated Sample
+               column(8, wellPanel(
+                 plotOutput('simulate1'),
+                 tableOutput('random_summary')
+               ))
+             ) 
+    ),
+    
+    
+    tabPanel("Binomial",
+             wellPanel(h2('The Binomial Density'),
+                       'The effect of the number of trials and the probability of success'),
+             fluidRow(
+               # Input for normal density plot
+               column(4, wellPanel(
+                 sliderInput('binomx', label='Vector of quantiles', min=-10, max=20, value=c(-5,10), step=1),
+                 sliderInput('binomsize', label='Number of trials', min=0, max=10, value=8, step=1),
+                 sliderInput('binomprob', label='Probability of success on each trial', min=0, max=1, value=0.5, step=.01),
+               )),
+               # Plot of normal density
+               column(8, wellPanel(
+                 plotOutput('binomdist')
+               )),
+               column(4, random_tab()
+               ),
+               # Plot of simulated Sample
+               column(8, wellPanel(
+                 plotOutput('simulate2'),
+                 #tableOutput('random_summary')
+               ))
+             )             
+    ),
+    
+    
+    tabPanel("Gamma",
+             wellPanel(h2('The Gamma Density'),
+                       'The effect of the number of trials and the probability of success'),
+             fluidRow(
+               # Input for normal density plot
+               column(4, wellPanel(
+                 sliderInput('gammashape', label='Shape and scale parameters', min=0, max=10, value=6, step=0.1),
+                 sliderInput('gammarate', label='Rate (Scale=1/Rate)', min=0, max=10, value=1, step=.1),
+               )),
+               # Plot of normal density
+               column(8, wellPanel(
+                 plotOutput('gammadist')
+               )),
+               column(4, random_tab()
+               ),
+               # Plot of simulated Sample
+               column(8, wellPanel(
+                 plotOutput('simulate3'),
+                 #tableOutput('random_summary')
+               ))
+             )
+    )
+  )  
 )
 
 
+
+
+
+# Function for type of the random sampling plot (Without input$do)
+plot_random_type <-function(rtype, input1, input2, input3) {
+  if (rtype == 'rnormal') {
+    rnorm(n=input1, mean=input2, sd=input3) #mean=input$mu, sd=input$sigma)
+  }
+  else if (rtype == 'rbinomial'){
+    rbinom(n=input1, size=input2, prob=input3) #size=input$binomsize, prob=input$binomprob
+  }
+  else if (rtype == 'rgamma'){
+    rgamma(n=input1, shape=input2, rate=input3)
+  }
+}
+
+
+
+
+# Function v.2 for type of the random sampling plot (With input$do)
+plot_random_type2 <-function(rtype, input0, input1, input2, input3) {
+  eventReactive(input0, {
+    if (rtype == 'rnormal') {
+      rnorm(n=input1, mean=input2, sd=input3) #mean=input$mu, sd=input$sigma)
+    }
+    else if (rtype == 'rbinomial'){
+      rbinom(n=input1, size=input2, prob=input3) #size=input$binomsize, prob=input$binomprob
+    }
+    else if (rtype == 'rgamma'){
+      rgamma(n=input1, shape=input2, rate=input3)
+    }    
+  }, ignoreNULL = FALSE)}
+
+
+
+
+
 server <- function(input, output) {
-  # Output normal distribution
+  # TAB 1 Output normal distribution
   output$normaldist <- renderPlot({
     x <- seq(from=-12, to=22, by=.1)
     y <- dnorm(x, mean=input$mu, sd=input$sigma)
@@ -57,16 +164,27 @@ server <- function(input, output) {
     }
   })
   
+  
   # Output of random button
   random_points <- eventReactive(input$do, {
-    rnorm(n=input$n, mean=input$mu, sd=input$sigma)
-  }, ignoreNULL = FALSE
-  ) 
+    plot_random_type('rnormal', input$n, input$mu, input$sigma)
+  }, ignoreNULL = FALSE)
+  
+  
+  
+  #random_points <- plot_random_type2('rnormal', input$do, input$n, input$mu, input$sigma)
+  
+  
+  
   # Add output of simulated sample plot and density
-  output$simulate <- renderPlot({
+  output$simulate1 <- renderPlot({
+    #par(oma = c(4, 0, 0, 0)) # to add margin under plot
     # Plot histogram of sample points
-    hist(random_points(), freq = FALSE, ylim=c(0, .4), xlim=c(-10,20),
+    hist(random_points(), freq=FALSE, ylim=c(0, .4), xlim=c(-10,20),
          ylab='Density', xlab='Random Samples', main='Plot of simulated sample')
+    #plot_text=paste("Mean = ", round(mean(x),3), ", SD = ", round(sd(x),3)) #To add mean=, sd= under plot
+    #mtext(text=plot_text, side=1, line=6, outer = FALSE) #To add mean=, sd= under plot
+    
     # Checkbox to show density curve overlayed on the histogram
     if(input$dens == TRUE){
       # Plot density curve overlayed on the histogram
@@ -77,18 +195,83 @@ server <- function(input, output) {
   # Output of summary
   output$random_summary <- renderTable({
     data.frame(
-      N = as.character(length(random_points())),
+      #      Name = c("Mean", "SD"),
+      #      Value = as.character(c(round(mean(random_points()),2), 
+      #                             round(sd(random_points()),2)
+      #                           ))
+      N = as.character(input$n),
       Median = as.character(round(median(random_points()),2)),
       Mean = as.character(round(mean(random_points()),2)),
       SD = as.character(round(sd(random_points()),2)),
       Min = as.character(round(min(random_points()),2)),
       Max = as.character(round(max(random_points()),2)),
-      Skweeness = as.character(round(moments::skewness(random_points()),2)),
-      Kurtosis = as.character(round(moments::kurtosis(random_points()),2))
+      Skweeness = as.character(round(skewness(random_points()),2)),
+      Kurtosis = as.character(round(kurtosis(random_points()),2))
     )
+  }, width = "100%")
+  
+  
+  
+  
+  
+  # TAB 2 Output binomial distribution
+  output$binomdist <- renderPlot({
+    y <- dbinom(x=input$binomx[1]:input$binomx[2], size=input$binomsize, prob=input$binomprob)
+    plot(y, type='l', ylim=c(0, 1), xlim = c(-10,30),
+         ylab='Density', main='Plot of Binomial density')
+  })   
+  
+  
+  # Output of random button
+  random_points2 <- eventReactive(input$do, {
+    plot_random_type('rbinomial', input$n, input$binomsize, input$binomprob)
+  }, ignoreNULL = FALSE)
+  
+  
+  
+  #random_points2 <- plot_random_type2('rbinomial', input$do, input$n, input$binomsize, input$binomprob)
+  
+  
+  
+  output$simulate2 <- renderPlot({
+    hist(random_points2(), freq=FALSE, #ylim=c(0, .4), xlim=c(-10,20),
+         ylab='Density', xlab='Random Samples', main='Plot of simulated sample')
     
-  }, width = "100%"
-  )
+    # Checkbox to show density curve overlayed on the histogram
+    if(input$dens == TRUE){
+      # Plot density curve overlayed on the histogram
+      lines(density(random_points(), bw=input$dens_smooth), col="red")
+    }
+  })
+  
+  
+  
+  
+  
+  # TAB 3 Output gamma distribution
+  output$gammadist <- renderPlot({
+    y <- dgamma(seq(0, 2, by = 0.04), shape = input$gammashape, rate=input$gammarate)
+    #    y <- dbinom(x=input$binomx[1]:input$binomx[2], size=input$binomsize, prob=input$binomprob)
+    plot(y, type='l', #ylim=c(0, 5), xlim = c(-10,50),
+         ylab='Density', main='Plot of Binomial density')
+  })   
+  
+  
+  # Output of random button
+  random_points3 <- eventReactive(input$do, {
+    plot_random_type('rgamma', input$n, input$gammashape, input$gammarate)
+  }, ignoreNULL = FALSE) 
+  
+  output$simulate3 <- renderPlot({
+    hist(random_points3(), freq=FALSE, #ylim=c(0, .4), xlim=c(-10,20),
+         ylab='Density', xlab='Random Samples', main='Plot of simulated sample')
+    
+    # Checkbox to show density curve overlayed on the histogram
+    if(input$dens == TRUE){
+      # Plot density curve overlayed on the histogram
+      lines(density(random_points(), bw=input$dens_smooth), col="red")
+    }
+  })
 }
 
 shinyApp(ui = ui, server = server, options=list(height=600))
